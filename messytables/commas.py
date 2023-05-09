c379e2902eacb71bd344a31babb641c19c650e8b
Pat Buxton <patrick.buxton@tartansolutions.com>
2023-05-09 13:33:57 +0100

    Custom CSV sniffer to fix bug

--- messytables/commas.py
+++ messytables/commas.py
@@ -1,5 +1,6 @@
 import csv
 import codecs
+import re
 import chardet
 
 from messytables.core import RowSet, TableSet, Cell
@@ -138,8 +139,10 @@ class CSVRowSet(RowSet):
         delim = '\n'  # NATIVE
         sample = delim.join(self._sample)
         try:
-            dialect = csv.Sniffer().sniff(sample,
-                delimiters=['\t', ',', ';', '|'])  # NATIVE
+            dialect = BetterSniffer().sniff(
+                sample,
+                delimiters='\t,;|'  # NATIVE
+            )
             dialect.delimiter = native_string(dialect.delimiter)
             dialect.quotechar = native_string(dialect.quotechar)
             dialect.lineterminator = delim
@@ -192,3 +195,81 @@ class CSVRowSet(RowSet):
                 pass
             else:
                 raise messytables.ReadError('Error reading CSV: %r', err)
+
+
+class BetterSniffer(csv.Sniffer):
+
+    def _guess_quote_and_delimiter(self, data, delimiters):
+        """
+        Looks for text enclosed between two identical quotes
+        (the probable quotechar) which are preceded and followed
+        by the same character (the probable delimiter).
+        For example:
+                         ,'some text',
+        The quote with the most wins, same with the delimiter.
+        If there is no quotechar the delimiter can't be determined
+        this way.
+        """
+
+        matches = []
+        for restr in (r'(?P<delim>[^\w\n"\'])(?P<space> ?)(?P<quote>["\'])[^\n]*?(?P=quote)(?P=delim)', # ,".*?",
+                      r'(?:^|\n)(?P<quote>["\'])[^\n]*?(?P=quote)(?P<delim>[^\w\n"\'])(?P<space> ?)',   #  ".*?",
+                      r'(?P<delim>[^\w\n"\'])(?P<space> ?)(?P<quote>["\'])[^\n]*?(?P=quote)(?:$|\n)',   # ,".*?"
+                      r'(?:^|\n)(?P<quote>["\'])[^\n]*?(?P=quote)(?:$|\n)'):                            #  ".*?" (no delim, no space)
+            regexp = re.compile(restr, re.DOTALL | re.MULTILINE)
+            matches = regexp.findall(data)
+            if matches:
+                break
+
+        if not matches:
+            # (quotechar, doublequote, delimiter, skipinitialspace)
+            return ('', False, None, 0)
+        quotes = {}
+        delims = {}
+        spaces = 0
+        groupindex = regexp.groupindex
+        for m in matches:
+            n = groupindex['quote'] - 1
+            key = m[n]
+            if key:
+                quotes[key] = quotes.get(key, 0) + 1
+            try:
+                n = groupindex['delim'] - 1
+                key = m[n]
+            except KeyError:
+                continue
+            if key and (delimiters is None or key in delimiters):
+                delims[key] = delims.get(key, 0) + 1
+            try:
+                n = groupindex['space'] - 1
+            except KeyError:
+                continue
+            if m[n]:
+                spaces += 1
+
+        quotechar = max(quotes, key=quotes.get)
+
+        if delims:
+            delim = max(delims, key=delims.get)
+            skipinitialspace = delims[delim] == spaces
+            if delim == '\n': # most likely a file with a single column
+                delim = ''
+        else:
+            # there is *no* delimiter, it's a single column of quoted data
+            delim = ''
+            skipinitialspace = 0
+
+        # if we see an extra quote between delimiters, we've got a
+        # double quoted format
+        dq_regexp = re.compile(
+                               r"((%(delim)s)|^)\W*%(quote)s[^%(delim)s\n]*%(quote)s[^%(delim)s\n]*%(quote)s\W*((%(delim)s)|$)" % \
+                               {'delim':re.escape(delim), 'quote':quotechar}, re.MULTILINE)
+
+
+
+        if dq_regexp.search(data):
+            doublequote = True
+        else:
+            doublequote = False
+
+        return (quotechar, doublequote, delim, skipinitialspace)
